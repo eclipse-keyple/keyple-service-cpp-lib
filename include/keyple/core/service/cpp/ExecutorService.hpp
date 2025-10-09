bd4882f7e707599ba2631c48d0da9b6605bbcc13
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2025-10-09 09:54:47 +0200

    refactor: remove unused mutex from ExecutorService

--- include/keyple/core/service/cpp/ExecutorService.hpp
+++ include/keyple/core/service/cpp/ExecutorService.hpp
@@ -14,7 +14,6 @@
 #pragma once
 
 #include <memory>
-#include <mutex>
 #include <thread>
 #include <typeinfo>
 #include <vector>
@@ -71,11 +70,6 @@ private:
      *
      */
     std::vector<std::shared_ptr<Job>> mPool;
-
-    /**
-     *
-     */
-    std::mutex mMutex;
 
     /**
      *
